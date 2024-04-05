6665ceb00d5494ca43a6321f0e3b1effade51fd4
Najm-Ur-Rehman <67186639+Najaam@users.noreply.github.com>
2024-04-04 22:37:48 -0700

    done bhae

--- Implementataion/Js/index.js
+++ Implementataion/Js/index.js
@@ -183,6 +183,35 @@ var hotels = [
     img: "https://images.unsplash.com/photo-1600011689032-8b628b8a8747?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     button: "Book Now",
   },
+  
+  {
+    name: "Al Jaddaf Hotel",
+    location: "UAE",
+    city: "ABU DHABI",
+    img: "https://images.unsplash.com/photo-1600011689032-8b628b8a8747?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    button: "Book Now",
+  },
+  {
+    name: "Al Jaddaf Hotel",
+    location: "UAE",
+    city: "ABU DHABI",
+    img: "https://images.unsplash.com/photo-1600011689032-8b628b8a8747?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    button: "Book Now",
+  },
+  {
+    name: "Al Jaddaf Hotel",
+    location: "UAE",
+    city: "ABU DHABI",
+    img: "https://images.unsplash.com/photo-1600011689032-8b628b8a8747?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    button: "Book Now",
+  },
+  {
+    name: "Al Jaddaf Hotel",
+    location: "UAE",
+    city: "ABU DHABI",
+    img: "https://images.unsplash.com/photo-1600011689032-8b628b8a8747?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    button: "Book Now",
+  },
 ];
 
 function createHotelCards(hotels) {
